9d2b025c14b91ce9f28ba3d82f5e6bf872e39fdf
MarkMutt <lleasy8@gmail.com>
2024-11-14 12:35:16 -0500

    erased the postcss to rid error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import {flatRoutes} from "remix-flat-routes"
 
 declare module "@remix-run/node" {
   interface Future {
@@ -18,6 +19,23 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
+      routes: async (defineRoutes) => {
+						return flatRoutes('routes', defineRoutes, {
+							ignoredRouteFiles: [
+								'.*',
+								'**/*.css',
+								'**/*.test.{js,jsx,ts,tsx}',
+								'**/__*.*',
+								// This is for server-side utilities you want to colocate
+								// next to your routes without making an additional
+								// directory. If you need a route that includes "server" or
+								// "client" in the filename, use the escape brackets like:
+								// my-route.[server].tsx
+								'**/*.server.*',
+								'**/*.client.*',
+							],
+						})
+					},
     }),
     tsconfigPaths(),
   ],
